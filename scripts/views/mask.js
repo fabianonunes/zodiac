0502dcb70731297e164a2ace8780ac17c4d35a39
Fabiano Nunes <fabiano@nunes.me>
2011-08-12 18:55:59 -0300

    related element in dragleave events; intersection start logic

--- scripts/views/mask.js
+++ scripts/views/mask.js
@@ -8,9 +8,10 @@
 			'dragover' : 'cancel'
 			, 'dragenter' : 'cancel'
 			, 'drop' : 'drop'
-			, 'dragover div.act' : 'cancel'
-			, 'dragenter div.act' : 'actionEnter'
-			, 'drop div.act' : 'actionDrop'
+			, 'dragleave div' : 'actionLeave'
+			, 'dragover div' : 'cancel'
+			, 'dragenter div' : 'actionEnter'
+			, 'drop div' : 'actionDrop'
 		},
 		
 		initialize: function(){
@@ -18,8 +19,22 @@
 		},
 
 		drop : function(evt){
+
+			var oEvt = evt.originalEvent
+				, dt = oEvt.dataTransfer
+				, files = dt.files
+				, reader = new FileReader();
+
+			reader.onload = function (event) {
+				$('.input').text(event.target.result);
+			};
+
+			reader.readAsText(files[0]);			
+
 			this.cancel(evt);
 			this.el.hide();
+			console.log(this.el);
+
 		},
 
 		cancel : function(evt){
@@ -28,23 +43,46 @@
 			return false;
 		},
 
-		actionEnter : function(evt){
-			
-			$('.act.over', this.el).removeClass('over');
+		actionEnter : function(evt){		
 			$(evt.target).addClass('over');
 			return this.cancel(evt);
 		},
 
 		actionLeave : function(evt){
-			evt.preventDefault();
-			evt.stopPropagation();
-			$(evt.target).removeClass('over');
-			return false;
+
+			var related = document.elementFromPoint(evt.clientX, evt.clientY);
+
+			if(related !== evt.target){
+				var inside = $.contains(evt.target, related);
+				!inside && $(evt.target).removeClass('over');
+			}
+			
 		},
 
 		actionDrop : function(evt){
+
+			var evt = evt.originalEvent
+			, dt = evt.dataTransfer
+			, files = dt.files
+			, reader = new FileReader();
+
+			reader.onload = function (event) {
+
+				var t = event.target.result.split('\n');
+				var i = $('.input').text().split('\n');
+
+				console.log(_.intersection(t, i));
+
+
+
+			};
+
+			reader.readAsText(files[0]);
+
 			$(evt.target).removeClass('over');
+
 			this.el.hide();
+
 		}
 
 	});
