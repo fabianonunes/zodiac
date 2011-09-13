c08289693c0552abb60e411172ef78537812b303
Fabiano Nunes <fabiano@nunes.me>
2011-09-13 11:47:01 -0300

    operations symbols in path panel

--- scripts/views/path.js
+++ scripts/views/path.js
@@ -1,26 +1,26 @@
 !function(){
 
+	var ops = {
+		union : '∪'
+		, intersection : '∩'
+		, difference : '∖'
+		, symmetric : '⊖'
+	};
+
 	app.PathView = Backbone.View.extend({
 
 		el: $('.path'),
-
 		template : 'path', 
 
 		initialize: function(){
-
 			_.bindAll(this, 'render');
-
 			this.collection.bind('change', this.render);
-
 		},
 
 		render : function(){
-console.log('rendering->');
 			this.empty();
-			app.template({ documents : this.collection.toJSON() }, this.template, this.el[0]);
+			app.template({ documents : this.collection.toJSON(), ops : ops }, this.template, this.el[0]);
 		},
-
-
 
 		empty : function(){
 			while(this.el[0].firstChild){
