89629acf109fa2f0c5f7b1d4099c821d4e2eae00
Fabiano Nunes <fabiano@nunes.me>
2011-10-16 17:18:05 -0200

    remove unused underscore and backbone libs

--- scripts/libs/backbone/backbone.js
+++
@@ -1,7 +0,0 @@
-define([
-	'order!libs/backbone/backbone-0.5.3'
-], function(){
-	_.noConflict();
-	$.noConflict();
-	return Backbone.noConflict();
-});
--- scripts/libs/underscore/underscore.js
+++
@@ -1,6 +0,0 @@
-define([
-	'order!libs/underscore/underscore-1.1.7'
-], function(){
-	return _;
-});
-
