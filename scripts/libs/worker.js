87129ea0b25beb21613d413ed2970ffa3640ee1b
Fabiano Nunes <fabiano@nunes.me>
2011-10-16 15:28:02 -0200

    realocate libs

---
+++ scripts/libs/worker.js
@@ -0,0 +1,18 @@
+
+define(function(){
+	return function(file, options, cb){
+	
+		if (window.Worker) {
+
+			var worker = new Worker(file);
+			worker.onmessage = function(event){
+				cb(event);
+				worker.terminate();
+			};
+
+			worker.postMessage(options);
+
+		}
+		
+	};
+});
---
+++ scripts/renderer.js
@@ -0,0 +1,19 @@
+define(['dust'], function(dust){
+	return function(data, template, el, cb){
+
+		if(cb) cb = cb.bind(null, el);
+
+		var s = document.createElement('span');
+		
+		dust.render(template, data, function(err, out) {
+
+			s.innerHTML = out;
+			
+			el.appendChild(s);
+
+			if(cb) cb();
+
+		});
+
+	};
+});
