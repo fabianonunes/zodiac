34eb03e71c800eca8c0e3945a24ca8e28f85727a
Fabiano Nunes <fabiano@nunes.me>
2011-12-21 12:56:46 -0200

    improve worker callbacks

--- public/scripts/libs/worker.js
+++ public/scripts/libs/worker.js
@@ -6,42 +6,36 @@ define(['libs/jqmq', 'jquery', 'underscore'], function (jqmq, $, _) {
 	var queue = jqmq({
 		delay    : -1,
 		batch    : 1,
-		callback : function callback (item) {
-
-			item.worker.onmessage = function onmessage (queue, event) {
-				this.worker.onmessage = null;
-				this.worker.onerror = null;
-				this.success(event);
-				queue.next();
-			}.bind(item, queue);
-
-			item.worker.onerror = function onerror (queue, event) {
-				this.worker.onmessage = null;
-				this.worker.onerror = null;
-				this.error(event);
-				queue.next();
-			}.bind(item, queue);
-
+		callback : function jqmqCallback (item) {
+			item.worker.onmessage = onresponse(item.worker, item.success, queue.next);
+			item.worker.onerror = onresponse(item.worker, item.error, queue.next);
 			item.worker.postMessage( item.optback() );
-
 		}
 	});
 
+	function onresponse (worker, resolve, next) {
+		return function onresp (event) {
+			worker.onmessage = null;
+			worker.onerror = null;
+			resolve(event);
+			next();
+		};
+	}
+
 	return function (file, optback) {
 
-		var defer = $.Deferred();
+		return $.Deferred(function workerDfd (defer) {
 
-		var worker = workers[file] || (workers[file] = new Worker(file));
-		// var worker = new Worker(file);
+			var worker = workers[file] || (workers[file] = new Worker(file));
 
-		queue.add({
-			worker  : worker,
-			optback : optback,
-			success : defer.resolve,
-			error   : defer.reject
+			queue.add({
+				worker  : worker,
+				optback : optback,
+				success : defer.resolve,
+				error   : defer.reject
+			});
+
 		});
-
-		return defer;
 
 	};
 
