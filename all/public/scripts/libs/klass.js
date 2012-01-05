61bd8b8aaaa03945ddbad1988d4071023c456891
Fabiano Nunes <fabiano@nunes.me>
2012-01-05 20:32:03 -0200

    bugfix - like publisher lib, requirejs optimization breakup class

--- all/public/scripts/libs/klass.js
+++ all/public/scripts/libs/klass.js
@@ -3,4 +3,90 @@
   * https://github.com/ded/klass
   * License MIT (c) Dustin Diaz & Jacob Thornton
   */
-!function(a,b){typeof define=="function"?define(b):typeof module!="undefined"?module.exports=b():this[a]=b()}("klass",function(){function f(a){return j.call(g(a)?a:function(){},a,1)}function g(a){return typeof a===c}function h(a,b,c){return function(){var d=this.supr;this.supr=c[e][a];var f=b.apply(this,arguments);return this.supr=d,f}}function i(a,b,c){for(var f in b)b.hasOwnProperty(f)&&(a[f]=g(b[f])&&g(c[e][f])&&d.test(b[f])?h(f,b[f],c):b[f])}function j(a,b){function c(){}function l(){this.initialize?this.initialize.apply(this,arguments):(b||h&&d.apply(this,arguments),j.apply(this,arguments))}c[e]=this[e];var d=this,f=new c,h=g(a),j=h?a:this,k=h?{}:a;return l.methods=function(a){return i(f,a,d),l[e]=f,this},l.methods.call(l,k).prototype.constructor=l,l.extend=arguments.callee,l[e].implement=l.statics=function(a,b){return a=typeof a=="string"?function(){var c={};return c[a]=b,c}():a,i(this,a,d),this},l}var a=this,b=a.klass,c="function",d=/xyz/.test(function(){xyz})?/\bsupr\b/:/.*/,e="prototype";return f.noConflict=function(){return a.klass=b,this},a.klass=f,f})
+!function (name, definition) {
+  if (typeof define == 'function') define(function(){ return definition(); })
+  else if (typeof module != 'undefined') module.exports = definition()
+  else this[name] = definition()
+}('klass', function () {
+  var context = this
+    , old = context.klass
+    , f = 'function'
+    , fnTest = /xyz/.test(function () {xyz}) ? /\bsupr\b/ : /.*/
+    , proto = 'prototype'
+
+  function klass(o) {
+    return extend.call(isFn(o) ? o : function () {}, o, 1)
+  }
+
+  function isFn(o) {
+    return typeof o === f
+  }
+
+  function wrap(k, fn, supr) {
+    return function () {
+      var tmp = this.supr
+      this.supr = supr[proto][k]
+      var ret = fn.apply(this, arguments)
+      this.supr = tmp
+      return ret
+    }
+  }
+
+  function process(what, o, supr) {
+    for (var k in o) {
+      if (o.hasOwnProperty(k)) {
+        what[k] = isFn(o[k])
+          && isFn(supr[proto][k])
+          && fnTest.test(o[k])
+          ? wrap(k, o[k], supr) : o[k]
+      }
+    }
+  }
+
+  function extend(o, fromSub) {
+    // must redefine noop each time so it doesn't inherit from previous arbitrary classes
+    function noop() {}
+    noop[proto] = this[proto]
+    var supr = this
+      , prototype = new noop()
+      , isFunction = isFn(o)
+      , _constructor = isFunction ? o : this
+      , _methods = isFunction ? {} : o
+    function fn() {
+      if (this.initialize) this.initialize.apply(this, arguments)
+      else {
+        fromSub || isFunction && supr.apply(this, arguments)
+        _constructor.apply(this, arguments)
+      }
+    }
+
+    fn.methods = function (o) {
+      process(prototype, o, supr)
+      fn[proto] = prototype
+      return this
+    }
+
+    fn.methods.call(fn, _methods).prototype.constructor = fn
+
+    fn.extend = arguments.callee
+    fn[proto].implement = fn.statics = function (o, optFn) {
+      o = typeof o == 'string' ? (function () {
+        var obj = {}
+        obj[o] = optFn
+        return obj
+      }()) : o
+      process(this, o, supr)
+      return this
+    }
+
+    return fn
+  }
+
+  klass.noConflict = function () {
+    context.klass = old
+    return this
+  }
+  context.klass = klass
+
+  return klass
+})
