eb8b1fd461e418cdf37af0293fec895c04feda65
Fabiano Nunes <fabiano@nunes.me>
2012-11-14 15:53:18 -0200

    remove deprecated blob constructor

--- all/app/public/scripts/lib/blob.js
+++ all/app/public/scripts/lib/blob.js
@@ -1,22 +1,21 @@
-/*global define, BlobBuilder*/
+/*global define, blob*/
 define([
-	'jquery',
-	'underscore'
-], function($, _){
+	'lib/q',
+	'underscore',
+	'jquery'
+], function(q, _, $){
 
-	window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder
+	"use strict";
 
 	var blob = {}
 
 	blob.createBlob = function createBlob (data) {
 
 		var path = _.uniqueId('selection_'),
-			bb   = new BlobBuilder(),
-			blob
+			blob = new Blob([data], { type : 'text/plain' })
 
-		bb.append(data)
-		blob = bb.getBlob('text/plain')
 		blob.name = path
+
 		return blob
 
 	}
@@ -36,7 +35,10 @@ define([
 		if ( length > 1 ) {
 			file = this.createBlob(names.join('\n'))
 		} else if ( length === 0 ) {
-			file = this.createBlob( dataTransfer.getData('text/plain') )
+			var type = _(dataTransfer.types).find(function(type){
+				return ~type.indexOf('text/plain') || ~type.indexOf('text/html')
+			})
+			file = this.createBlob(dataTransfer.getData(type))
 		} else {
 			file = dataTransfer.files[0]
 		}
@@ -45,29 +47,29 @@ define([
 
 	}
 
-	blob.readBlob = function (file) {
-		return $.Deferred(function (dfd) {
-			var reader = new FileReader()
-			reader.onload = function (event) {
-				dfd.resolve(event.target.result)
-			}
-			reader.readAsText(file)
+	blob.readBlob = function (file, charset) {
+		var dfd = q.defer()
+		var reader = new FileReader(), $r = $(reader)
+		$r.on('load', function (event) {
+			dfd.resolve(event.target.result)
+			$r.off()
 		})
+		$r.on('error', function (err) {
+			dfd.reject(err)
+			$r.off()
+		})
+		reader.readAsText(file, charset ? charset : null) // firefox bug : if charset is undefined, the file isn't read
+		return dfd.promise
 	}
 
 	blob.downloadURL = function (file, name) {
 		var mime = file.type
-		return $.Deferred(function (dfd) {
-			blob.readBlob(file).then(function (event) {
-				var data = event.target.result
-				dfd.resolve(mime+':'+name+'.txt:data:'+mime+'base64,'+window.btoa(data))
-			})
+		return blob.readBlob(file).then(function (event) {
+			var data = event.target.result
+			return mime+':'+name+'.txt:data:'+mime+'base64,'+window.btoa(data)
 		})
 	}
 
 	return blob
 
 })
-
-
-
